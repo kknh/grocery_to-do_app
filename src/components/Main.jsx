c534afbb9d53b3bfd9371f37512cc0c45bf2730f
kknh <kimkunho@live.com>
2022-05-15 13:02:01 +0200

    add alert

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import './Main.css'
 import List from './List'
 import Alert from './Alert'
